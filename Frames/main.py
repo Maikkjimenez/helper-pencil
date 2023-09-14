41ee3da0090cf14f20794f08a50531f27ba113af
Angie Duarte M <80783234+AngieMatson@users.noreply.github.com>
2023-09-14 13:18:09 -0500

    Primer Frame al 90%, faltó minimizar el size

--- Frames/main.py
+++ Frames/main.py
@@ -1,19 +1,29 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
-from kivy.config import Config
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
 
-Config.set('graphics', 'width', '800')
-Config.set('graphics', 'height', '600')
+Window.size = (370,670)
 
 Builder.load_file("home.kv")
 
-class HomeScreen(MDScreen):
-    pass
+class MyScreenManager(ScreenManager):
+	pass
+
+class FirstScreen(Screen):
+	pass
+
+class MyButton(Widget):
+	pass
+
+class SecondScreen(Screen):
+	pass
 
 class MyApp(MDApp):
     def build(self):
-        return HomeScreen()
+        return MyScreenManager()
 
 if __name__ == '__main__':
     MyApp().run()
