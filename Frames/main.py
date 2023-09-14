3d6171991de5a6605011916b86216031ef316063
Angie Duarte M <80783234+AngieMatson@users.noreply.github.com>
2023-09-14 15:25:14 -0500

    New

--- Frames/main.py
+++ Frames/main.py
@@ -12,18 +12,32 @@ Builder.load_file("home.kv")
 class MyScreenManager(ScreenManager):
 	pass
 
-class FirstScreen(Screen):
-	pass
-
 class MyButton(Widget):
 	pass
 
+class FirstScreen(Screen):
+	pass
 class SecondScreen(Screen):
 	pass
+class ThirdScreen(Screen):
+	pass
+class FourthScreen(Screen):
+	pass
+class FifthScreen(Screen):
+	pass
+class SixthScreen(Screen):
+	pass
+
 
 class MyApp(MDApp):
     def build(self):
-        return MyScreenManager()
+       	sm = ScreenManager()
+        sm.add_widget(FirstScreen(name='first_screen'))
+        sm.add_widget(SecondScreen(name='second_screen'))
+        return sm
+
+    def change_screen(self, screen_name):
+      	self.root.current = screen_name
 
 if __name__ == '__main__':
     MyApp().run()
